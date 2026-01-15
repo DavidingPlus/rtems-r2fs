08260aca8152b2dfe78c1eec8f587c0f7bcb634b
DavidingPlus <davidingplus@qq.com>
2026-01-15 08:04:18 +0000

    add log and exception for utils

---
+++ src/utils/r2fs_log.c
@@ -0,0 +1,36 @@
+#include "r2fs_log.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <string.h>
+
+
+const char *log_level_str[] = {"DEBUG", "INFO", "WARING", "ERROR"};
+#define ERRNO_MSG_LEN 128
+#define PRINT_TO_STDERR(fmt, ...) \
+    vfprintf(stderr, fmt, ##__VA_ARGS__)
+
+
+void r2fs_log_print(r2fs_log_level log_level, const char *funcname, unsigned int lineno, const char *fmt, ...)
+{
+    fprintf(stderr, "[%s:%u, %s]: ", funcname, lineno, log_level_str[log_level]);
+
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+}
+
+void r2fs_log_errno(r2fs_log_level log_level, const char *funcname, unsigned int lineno, int err, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    r2fs_log_print(log_level, funcname, lineno, fmt, args);
+    va_end(args);
+
+    char errno_msg[ERRNO_MSG_LEN];
+    if (strerror_r(err, errno_msg, sizeof(errno_msg)) == 0)
+        fprintf(stderr, "error: %s\n", errno_msg);
+}
---
+++ src/utils/r2fs_log.h
@@ -0,0 +1,33 @@
+#ifndef _R2FS_LOG_H_
+#define _R2FS_LOG_H_
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef enum r2fs_log_level
+    {
+        R2FS_LOG_DEBUG,
+        R2FS_LOG_INFO,
+        R2FS_LOG_WARNING,
+        R2FS_LOG_ERROR
+    } r2fs_log_level;
+
+    void r2fs_log_print(r2fs_log_level log_level, const char *funcname, unsigned int lineno, const char *fmt, ...);
+    void r2fs_log_errno(r2fs_log_level log_level, const char *funcname, unsigned int lineno, int err, const char *fmt, ...);
+
+#define R2FS_LOG(log_level, format, ...) \
+    r2fs_log_print(log_level, __func__, __LINE__, format, ##__VA_ARGS__)
+
+#define R2FS_ERRNO_LOG(log_level, err, format, ...) \
+    r2fs_log_errno(log_level, __func__, __LINE__, err, format, ##__VA_ARGS__)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
